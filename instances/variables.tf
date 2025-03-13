505871f6e84aea4a3ffa241ecf4fa6d4d29266fc
cloud-246 <cloudlk246@gmail.com>
2025-03-13 15:46:53 +0530

    uploading instances module

---
+++ instances/variables.tf
@@ -0,0 +1,6 @@
+variable "public_1_id" {}
+variable "bastion_host_sg_id" {}
+variable "key_name" {}  #"public_instance_key.pem"
+variable "ami" {} #"ami-08b5b3a93ed654d19"
+variable "private_1_id" {}
+variable "private_instance_sg_id" {}
