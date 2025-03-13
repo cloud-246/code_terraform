f7dc79595453aa8e2b1a8dc5611e1e10fbf3c96e
cloud-246 <cloudlk246@gmail.com>
2025-03-13 16:16:59 +0530

    Delete load_balancer directory

--- load_balancer/variables.tf
+++
@@ -1,9 +0,0 @@
-
-variable "lb_security_group_id" {}
-variable "load_balancer_subnets" {}
-
-
-variable "tg_vpc_id" {}
-variable "target_id" {}
-variable "listener_port" {}
-variable "listener_protocol" {}
